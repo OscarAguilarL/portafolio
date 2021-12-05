5ed8ec481411780c33ba9d8feec7361e019f5fdb
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-12-05 14:05:27 -0600

    Create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import styles from './Button.module.css';
+
+interface ButtonProps {
+    text: string;
+    type: 'primary' | 'secondary';
+    href: string;
+    download: boolean;
+}
+
+export const Button = ({ text, type, href, download = false }: ButtonProps) => {
+    return (
+        <a
+            href={href}
+            className={`${styles.button} ${styles[type]}`}
+            download={download}
+        >
+            {text}
+        </a>
+    );
+};
